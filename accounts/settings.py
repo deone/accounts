54c882180ecc1975b82b9afb977d433b18dae7d2
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-21 19:45:46 +0000

    Use MySQL.

--- accounts/settings.py
+++ accounts/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'accounts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'accounts',
+        'USER': 'accounts_user',
+        'PASSWORD': 'Acc0untS',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
---
+++ db.sql
@@ -0,0 +1,2 @@
+create database accounts;
+grant all on accounts.* to 'accounts_user'@'localhost' identified by 'Acc0untS';

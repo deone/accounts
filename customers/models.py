bcf19ae7f27ee00b49b3e2611f8c2ae06a1f500c
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-23 17:48:57 +0000

    Add __str__ functions.

--- customers/models.py
+++ customers/models.py
@@ -13,15 +13,27 @@ def generate_code(size=4, chars=string.digits):
 
 class Service(models.Model):
     name = models.CharField(_('name'), max_length=20, unique=True)
+    
+    def __str__(self):
+        return self.name
 
 class Customer(models.Model):
     name = models.CharField(_('name'), max_length=80, unique=True)
     code = models.CharField(_('code'), max_length=4, default=generate_code, editable=False, unique=True)
+    
+    def __str__(self):
+        return '%s %s' % (self.name, self.code)
 
 class CustomerUser(models.Model):
     user = models.OneToOneField(User)
     customer = models.ForeignKey(Customer)
+    
+    def __str__(self):
+        return '%s, %s' % (self.user.get_full_name(), self.customer.name)
 
 class CustomerService(models.Model):
     customer = models.ForeignKey(Customer)
     service = models.ForeignKey(Service)
+    
+    def __str__(self):
+        return '%s - %s' % (self.customer.name, self.service.name)

219a2996656ecdbc6ff89f9a4df01c84a2006fce
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-23 17:41:59 +0000

    Create models Customer, CustomerService and CustomerUser.

--- customers/models.py
+++ customers/models.py
@@ -11,17 +11,17 @@ import string
 def generate_code(size=4, chars=string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-""" class Service(models.Model):
-    pass """
+class Service(models.Model):
+    name = models.CharField(_('name'), max_length=20, unique=True)
 
 class Customer(models.Model):
     name = models.CharField(_('name'), max_length=80, unique=True)
     code = models.CharField(_('code'), max_length=4, default=generate_code, editable=False, unique=True)
 
-""" class CustomerUser(models.Model):
+class CustomerUser(models.Model):
     user = models.OneToOneField(User)
     customer = models.ForeignKey(Customer)
 
 class CustomerService(models.Model):
     customer = models.ForeignKey(Customer)
-    service = models.ForeignKey(Service) """
+    service = models.ForeignKey(Service)
